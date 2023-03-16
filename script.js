609230cd86ebe66abff9b2168fe69fcb72ac3b50
Juan Antonio López <juanantonio8686@outlook.com>
2023-03-16 12:44:56 +0100

    code

---
+++ script.js
@@ -0,0 +1,16 @@
+let size = 8;
+
+let board = "";
+
+for (let i = 0; i < size; i++) {
+    for (let x = 0; x < size; x++) {
+        if ((x + i) % 2 == 0) {
+            board = board + " ";
+        } else {
+            board = board + "#";
+        }
+    }
+    board = board + "\n";
+}
+
+console.log(board);
